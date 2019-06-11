6db543dafa2fb4b679489cb631d7154868dbcbab
Comfortok <olzhas.hasenov@yandex.kz>
2019-06-11 23:35:39 +0600

    added new version of morse code translator

---
+++ practice03morsecode/MorseToSymbolViceVersaTranslator.java
@@ -0,0 +1,139 @@
+package kz.epam.khassenov.practice03morsecode;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class MorseToSymbolViceVersaTranslator {
+    public static void main(String[] args) {
+        try (BufferedReader inputFile = new BufferedReader(new FileReader("C:\\Users\\Olzhas\\IdeaProjects\\" +
+                "EpamPractice\\src\\kz\\epam\\khassenov\\practice03morsecode\\InputFile.txt"));
+             BufferedWriter outputFile = new BufferedWriter(new FileWriter("C:\\Users\\Olzhas\\IdeaProjects\\" +
+                     "EpamPractice\\src\\kz\\epam\\khassenov\\practice03morsecode\\OutputFile.txt"))) {
+            String stringLine = inputFile.readLine();
+            if (String.valueOf(stringLine.charAt(0)).matches("\\w+$")){
+                convertSymbolToMorse(stringLine, outputFile);
+            }
+            else if (String.valueOf(stringLine.charAt(0)).matches("\\W+$")){
+                convertMorseToSymbol(stringLine, outputFile);
+            }
+            else {
+                System.out.println("Invalid text format!");
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String returnCharFromText(HashMap<String, String> hashMap, String inputChar) {
+        String result = null;
+        for (Map.Entry<String, String> pair : hashMap.entrySet()) {
+            if (pair.getKey().equals(inputChar)) {
+                result = pair.getValue();
+            } else if (pair.getValue().equals(inputChar)) {
+                result = pair.getKey();
+            } else {
+            }
+        }
+        return result;
+    }
+
+    public static boolean findChar(HashMap<String, String> hashMap, String inputChar) {
+        boolean result = false;
+        for (Map.Entry<String, String> pair : hashMap.entrySet()) {
+            if (pair.getKey().equals(inputChar)) {
+                result = true;
+            } else if (pair.getValue().equals(inputChar)) {
+                result = true;
+            } else {
+            }
+        }
+        return result;
+    }
+
+    public static HashMap<String, String> hashMapInitialization(HashMap<String, String> hashMap){
+        hashMap.put(".-", "A");
+        hashMap.put("-...", "B");
+        hashMap.put("-.-.", "C");
+        hashMap.put("-..", "D");
+        hashMap.put(".", "E");
+        hashMap.put("..-.", "F");
+        hashMap.put("--.", "G");
+        hashMap.put("....", "H");
+        hashMap.put("..", "I");
+        hashMap.put(".---", "J");
+        hashMap.put("-.-", "K");
+        hashMap.put(".-..", "L");
+        hashMap.put("--", "M");
+        hashMap.put("-.", "N");
+        hashMap.put("---", "O");
+        hashMap.put(".--.", "P");
+        hashMap.put("--.-", "Q");
+        hashMap.put(".-.", "R");
+        hashMap.put("...", "S");
+        hashMap.put("-", "T");
+        hashMap.put("..-", "U");
+        hashMap.put("...-", "V");
+        hashMap.put(".--", "W");
+        hashMap.put("-..-", "X");
+        hashMap.put("-.--", "Y");
+        hashMap.put("--..", "Z");
+        hashMap.put(".----", "1");
+        hashMap.put("..---", "2");
+        hashMap.put("...--", "3");
+        hashMap.put("....-", "4");
+        hashMap.put(".....", "5");
+        hashMap.put("-....", "6");
+        hashMap.put("--...", "7");
+        hashMap.put("---..", "8");
+        hashMap.put("----.", "9");
+        hashMap.put("-----", "0");
+        return hashMap;
+    }
+
+    public static void convertMorseToSymbol(String inputString, BufferedWriter outputFile) throws IOException {
+        HashMap<String, String> hashMapOfMorseAndSymbol = new HashMap<>();
+        hashMapInitialization(hashMapOfMorseAndSymbol);
+        StringBuffer inputFileTextOfMorse = new StringBuffer();
+        ArrayList<String> arrayListOfOutputSymbols = new ArrayList<>();
+        Scanner scanner = new Scanner(String.valueOf(inputString));
+        scanner.useDelimiter(";");
+        while (scanner.hasNext()){
+            String oneMorseString = scanner.next();
+            inputFileTextOfMorse.append(oneMorseString + " ");
+        }
+        String[] arrayOfMorseCode = String.valueOf(inputFileTextOfMorse).split(" ");
+        for (int i = 0; i < arrayOfMorseCode.length; i++){
+            if (findChar(hashMapOfMorseAndSymbol, arrayOfMorseCode[i])){
+                arrayListOfOutputSymbols.add(returnCharFromText(hashMapOfMorseAndSymbol, arrayOfMorseCode[i]));
+            }
+        }
+        for (String arrayElement : arrayListOfOutputSymbols) {
+            outputFile.write(arrayElement);
+        }
+    }
+
+    public static void convertSymbolToMorse(String inputString, BufferedWriter outputFile) throws IOException {
+        HashMap<String, String> hashMapOfMorseAndSymbol = new HashMap<>();
+        hashMapInitialization(hashMapOfMorseAndSymbol);
+        ArrayList<String> arrayListOfOutputSymbol = new ArrayList<>();
+        ArrayList<String> arrayListOfOutputMorse = new ArrayList<>();
+
+        for (int i = 0; i < inputString.length(); i++){
+            arrayListOfOutputSymbol.add(String.valueOf(inputString.charAt(i)));
+        }
+
+        int sizeOfSymbolArray = arrayListOfOutputSymbol.size();
+        for (int i = 0; i < sizeOfSymbolArray; i++){
+            if (findChar(hashMapOfMorseAndSymbol, arrayListOfOutputSymbol.get(i))){
+                arrayListOfOutputMorse.add(returnCharFromText(hashMapOfMorseAndSymbol, arrayListOfOutputSymbol.get(i)));
+            }
+        }
+        for (String arrayElement : arrayListOfOutputMorse) {
+            outputFile.write(arrayElement + " ");
+        }
+    }
+}
