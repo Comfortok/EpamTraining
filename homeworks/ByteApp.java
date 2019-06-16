22709a0a8fde0c425a53f80db3532969604b26f2
Comfortok <olzhas.hasenov@yandex.kz>
2019-06-16 19:58:28 +0600

    commit on hw package

--- homeworks/ByteApp.java
+++ homeworks/ByteApp.java
@@ -1,31 +1,6 @@
 package kz.epam.khassenov.homeworks;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class ByteApp {
-    public static void main(String[] args) throws IOException {
-        /*BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("Enter the first number: ");
-        String firstNumber = bufferedReader.readLine();
-        System.out.print("Enter the second number: ");
-        String secondNumber = bufferedReader.readLine();
-        if (firstNumber.contains(secondNumber)){
-            System.out.println("yes");
-        }else{
-            System.out.println("no");
-        }*/
-        Pattern pattern101 = Pattern.compile("1*01");
-        Pattern pattern1001 = Pattern.compile("1*001");
-
-        Matcher matcher101 = pattern101.matcher("101");
-        boolean is101 = matcher101.matches();
-
-        Matcher matcher1001 = pattern1001.matcher("10001");
-        boolean is1001 = matcher1001.matches();
-        System.out.println(is101 + " : " + is1001);
+    public static void main(String[] args) {
     }
 }
--- homeworks/ByteAppFirst.java
+++ homeworks/ByteAppFirst.java
@@ -1,6 +1,5 @@
 package kz.epam.khassenov.homeworks;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class ByteAppFirst {
